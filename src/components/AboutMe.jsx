bdace400bd67229dea99ba82281cf16c2ad6957a
Laura Ortiz <bjtamyl@hotmail.com>
2023-08-25 15:32:42 +0200

    about section model and model of cards of wedding page

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from "react";
+import rodrigoProfile from "../assets/rodrigoProfile.jpg"
+import about from "../css/about.css"
 
 const AboutMe = () => {
   return (
-    <div>AboutMe</div>
+    <div id="circle-shape-example">
+      <img src={rodrigoProfile} alt=""className="curve" />
+  <h1>Sobre Mí</h1>
+  <p>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum, quas. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore tempore illum aperiam id soluta ratione dolorum sapiente consectetur deserunt possimus. Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus dolorem, reiciendis natus atque libero maiores. Voluptas dignissimos in non error.
+  </p>
+  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Est commodi recusandae corrupti repudiandae fugit vero debitis nobis illo, amet laborum?</p>
+  </div>
   )
-}
+};
 
-export default AboutMe
+export default AboutMe;
--- src/pages/Weddings.jsx
+++ src/pages/Weddings.jsx
@@ -1,9 +1,150 @@
-import React from 'react'
+import React from "react";
+import NavBar from "../components/NavBar";
+import weddings from "../css/weddings.css";
+import pau from "../assets/pau.jpg"
+import q from "../assets/q.jpg";
+import q1 from "../assets/q1.jpg";
+import q2 from "../assets/q2.jpg";
+import q3 from "../assets/q3.jpg";
 
 const Weddings = () => {
   return (
-    <div>Weddings</div>
-  )
-}
+    <>
+      <NavBar />
+  
+     
 
-export default Weddings
+      <section class="info">
+        <h1>Bodas</h1>
+        <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores cupiditate omnis accusantium doloremque quidem fugiat voluptas laudantium, officiis voluptatem magnam?</h3>
+      </section>
+      <section className="cards-wrapper">
+        <div className="card-grid-space">
+          <div className="card cardW card1">
+            <a href="https://codetheweb.blog/2017/10/06/html-syntax/">
+              <div>
+                <h3 className="cardtitle">Carla & Francisco</h3>
+              
+              </div>
+            </a>
+          </div>
+          
+        </div>
+        <div className="card-grid-space">
+          <div className="card cardW card2">
+            <a
+              className=""
+              href="https://codetheweb.blog/2017/10/09/basic-types-of-html-tags/"
+            >
+              <div>
+                <h3 className="cardtitle">Basic types of HTML tags</h3>
+             
+              </div>
+            </a>
+          </div>
+        
+        </div>
+        <div className="card-grid-space">
+          <div className="card cardW card3">
+            <a
+              className=""
+              href="https://codetheweb.blog/2017/10/14/links-images-about-file-paths/"
+            >
+              <div>
+                <h3 className="cardtitle">Links, images and about file paths</h3>
+               
+              </div>
+            </a>
+          </div>
+          
+      
+        </div>
+        {/* ------------------------- */}
+        <div className="card-grid-space">
+          <div className="card cardW card1">
+            <a href="https://codetheweb.blog/2017/10/06/html-syntax/">
+              <div>
+                <h3 className="cardtitle">Carla & Francisco</h3>
+              
+              </div>
+            </a>
+          </div>
+          
+        </div>
+        <div className="card-grid-space">
+          <div className="card cardW card2">
+            <a
+              className=""
+              href="https://codetheweb.blog/2017/10/09/basic-types-of-html-tags/"
+            >
+              <div>
+                <h3 className="cardtitle">Basic types of HTML tags</h3>
+             
+              </div>
+            </a>
+          </div>
+        
+        </div>
+        <div className="card-grid-space">
+          <div className="card cardW card3">
+            <a
+              className=""
+              href="https://codetheweb.blog/2017/10/14/links-images-about-file-paths/"
+            >
+              <div>
+                <h3 className="cardtitle">Links, images and about file paths</h3>
+               
+              </div>
+            </a>
+          </div>
+          
+      
+        </div>
+        {/* -------------------------------- */}
+        <div className="card-grid-space">
+          <div className="card cardW card1">
+            <a href="https://codetheweb.blog/2017/10/06/html-syntax/">
+              <div>
+                <h3 className="cardtitle">Carla & Francisco</h3>
+              
+              </div>
+            </a>
+          </div>
+          
+        </div>
+        <div className="card-grid-space">
+          <div className="card cardW card2">
+            <a
+              className=""
+              href="https://codetheweb.blog/2017/10/09/basic-types-of-html-tags/"
+            >
+              <div>
+                <h3 className="cardtitle">Basic types of HTML tags</h3>
+             
+              </div>
+            </a>
+          </div>
+        
+        </div>
+        <div className="card-grid-space">
+          <div className="card cardW card3">
+            <a
+              className=""
+              href="https://codetheweb.blog/2017/10/14/links-images-about-file-paths/"
+            >
+              <div>
+                <h3 className="cardtitle">Links, images and about file paths</h3>
+               
+              </div>
+            </a>
+          </div>
+          
+      
+        </div>
+      
+      </section>
+    </>
+  );
+};
+
+export default Weddings;
